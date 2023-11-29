5ef6e50518918c282c5b35565e0b9a02a895332e
waqasyaqoob87 <92632689+waqasyaqoob87@users.noreply.github.com>
2023-11-29 16:19:10 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req,res)=>{
     res.render('index', {
-        title:'NODEJS Weather App Using XAmpp server',
+        title:'NODEJS Weather App Using Xampp server',
         name: 'Waqas Yaqoob'
     })
 })
